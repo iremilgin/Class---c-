fa3843662dae1f79462170bc8ef67e892b2adc36
Irem ILGIN <123415400+iremilgin@users.noreply.github.com>
2023-05-05 22:08:53 +0300

    Add project files.

---
+++ OgrenciUygulamasıClass/Ogrenci.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciUygulamasıClass
+{
+
+    public class Ogrenci
+    {
+        private int ogrno;
+
+        private string isim;
+
+        private string soyisim;
+
+        private int vize1;
+
+        private int vize2;
+
+        private int final;
+
+        private string okulIsmi;
+
+
+        public void OgrenciBilgileriGoster()
+        {
+            Console.WriteLine("Öğrenci Numarası: " + ogrno);
+            Console.WriteLine("Öğrenci İsmi " + isim);
+            Console.WriteLine("Öğrenci Soyismi: " + soyisim);
+            Console.WriteLine("Öğrenci Vize1: " + vize1);
+            Console.WriteLine("Öğrenci Vize2: " + vize2);
+            Console.WriteLine("Öğrenci Final " + final);
+            Console.WriteLine("Öğrenci Okul İsmi: " + okulIsmi);
+        }
+
+        public Ogrenci(int _ogrno, string _isim, string _soyIsim , int _vize1 , int _vize2, int _final , string _okulIsmi )
+        {
+
+            ogrno = _ogrno;
+            isim = _isim;   
+            soyisim = _soyIsim;
+            vize1 = _vize1;
+            vize2= _vize2;
+            final = _final;
+            okulIsmi= _okulIsmi;    
+
+        }
+
+
+
+        public double OgrenciNotuBul()
+        {
+           double ortalama = vize1 * 0.2 + vize2 * 0.2 + final * 0.6;
+            return ortalama;
+        }
+
+        public void OkulGetir()
+        {
+            Console.WriteLine("Öğrencinin Okulu: " + okulIsmi);
+        }
+
+    }
+}
---
+++ OgrenciUygulamasıClass/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace OgrenciUygulamasıClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kontrol = true;
+            Ogrenci ogrenci1 = new Ogrenci(7819 , "İrem" , "İlgin" , 95 , 85 , 78 , "İstanbul Bilgi Üniversitesi");
+
+            Console.WriteLine("Uygulamamıza Hoşgeldiniz... Yapmak istediğiniz işlemi seçiniz");
+            IslemleriGoster();
+
+
+            while (kontrol) 
+            {
+
+                string secim = Console.ReadLine();
+
+                switch (secim)
+                {
+                        case "1":
+                        ogrenci1.OgrenciBilgileriGoster();
+                        break;
+
+                        case "2":
+                        double ogrenciOrtalama =  ogrenci1.OgrenciNotuBul();
+                        Console.WriteLine("Öğrenci Ortalaması: " + ogrenciOrtalama);
+                        break;
+
+
+                        case "3":
+                        ogrenci1.OkulGetir();
+                        break;
+
+                        
+                        case "4":   
+                        kontrol = false;    
+                        break;
+                }
+
+            }
+           
+        }
+
+
+        static void IslemleriGoster()
+        {
+            Console.WriteLine("1- Öğrenci Bilgileri göster");
+            Console.WriteLine("2- Öğrenci Ortalamasını Göster");
+            Console.WriteLine("3- Öğrenci Okulunu Göster");
+            Console.WriteLine("4- Çıkış yap");
+        }
+    }
+}
